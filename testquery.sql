4140897b9c89f8f44101940966731fe7f69af1fb
Rasmus Arnmark <rasmusarnmark@Rasmuss-MacBook-Pro-2.local>
2025-03-27 08:28:06 +0100

    hmmhm

--- testquery.sql
+++ testquery.sql
@@ -7,7 +7,7 @@ CREATE FUNCTION has_unpaid_fine( vuserid VARCHAR(15) )
 RETURNS INT
 BEGIN
     DECLARE unpaid_fine INT;
-    SELECT COUNT(*) INTO unpaid_fine
+    SELECT CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END INTO unpaid_fine
     FROM fine
     WHERE userId = vuserid AND paid = 0;
     RETURN unpaid_fine;
