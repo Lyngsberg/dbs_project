ae50093206ce7ab6c1f1cee34fd5b27a50a3665c
Rasmus Arnmark <rasmusarnmark@Rasmuss-MacBook-Pro-2.local>
2025-03-27 08:44:11 +0100

    hmhm

--- testquery.sql
+++ testquery.sql
@@ -3,6 +3,31 @@ LEFT JOIN bookcopies bk ON bo.book_id = bk.book_id
 LEFT JOIN book b ON bk.ISBN = b.ISBN
 LEFT JOIN users u ON bo.userId = u.userid;
 
+SELECT b.*, u.* from borrowed bo
+LEFT JOIN bookcopies bk ON bo.book_id = bk.book_id
+LEFT JOIN book b ON bk.ISBN = b.ISBN
+LEFT JOIN users u ON bo.userId = u.userid
+WHERE bo.borrowed_at > '2023-01-01 00:00:00'
+GROUP BY bo.userId
+HAVING COUNT(*) > 1;
+
+SELECT b.*, u.* from borrowed bo
+LEFT JOIN bookcopies bk ON bo.book_id = bk.book_id
+LEFT JOIN book b ON bk.ISBN = b.ISBN
+LEFT JOIN users u ON bo.userId = u.userid
+WHERE bo.borrowed_at > '2023-01-01 00:00:00'
+GROUP BY bo.userId
+HAVING COUNT(*) > 1
+UNION
+SELECT b.*, u.* from borrowed bo
+LEFT JOIN bookcopies bk ON bo.book_id = bk.book_id
+LEFT JOIN book b ON bk.ISBN = b.ISBN
+LEFT JOIN users u ON bo.userId = u.userid
+WHERE bo.borrowed_at > '2023-01-01 00:00:00'
+GROUP BY bo.userId
+HAVING COUNT(*) > 1;
+
+
 CREATE FUNCTION has_unpaid_fine( vuserid VARCHAR(15) )
 RETURNS INT
 BEGIN
@@ -13,8 +38,6 @@ BEGIN
     RETURN unpaid_fine;
 END;
 
-
-
 CREATE TRIGGER Manual_Insert
     BEFORE INSERT
 ON borrowed FOR EACH ROW
@@ -22,8 +45,6 @@ BEGIN
     SIGNAL SQLSTATE 'HY000'
     SET MYSQL_ERRNO = 9998, MESSAGE_TEXT = 'Must use procedure InsertBorrowed';
 END;
-
-
 
 create Procedure InsertBorrowed
     (IN book_id int, IN userId int, IN borrowed_at TIMESTAMP, IN expiration_date TIMESTAMP, IN returned_at TIMESTAMP)
