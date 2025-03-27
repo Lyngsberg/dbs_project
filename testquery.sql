ed8cc5190a0a14b49a4861e423c30af79bfa4ae6
Rasmus Arnmark <rasmusarnmark@Rasmuss-MacBook-Pro-2.local>
2025-03-27 08:23:57 +0100

    function

--- testquery.sql
+++ testquery.sql
@@ -2,3 +2,13 @@ SELECT b.*, u.* from borrowed bo
 LEFT JOIN bookcopies bk ON bo.book_id = bk.book_id 
 LEFT JOIN book b ON bk.ISBN = b.ISBN
 LEFT JOIN users u ON bo.userId = u.userid;
+
+CREATE FUNCTION has_unpaid_fine( vuserid VARCHAR(15) )
+RETURNS INT
+BEGIN
+    DECLARE unpaid_fine INT;
+    SELECT COUNT(*) INTO unpaid_fine
+    FROM fine
+    WHERE userId = vuserid AND paid = 0;
+    RETURN unpaid_fine;
+END;
