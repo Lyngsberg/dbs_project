1c738a71e853bfb1c8bddd2a749c0aa18c967fa4
lyngsberg <Eliasrasmussen12345@gmail.com>
2025-03-27 08:28:59 +0100

    Eyo

--- testquery.sql
+++ testquery.sql
@@ -12,3 +12,23 @@ BEGIN
     WHERE userId = vuserid AND paid = 0;
     RETURN unpaid_fine;
 END;
+
+
+
+CREATE TRIGGER Manual_Insert
+    BEFORE INSERT
+ON borrowed FOR EACH ROW
+BEGIN
+    SIGNAL SQLSTATE 'HY000'
+    SET MYSQL_ERRNO = 9998, MESSAGE_TEXT = 'Must use procedure InsertBorrowed';
+END;
+
+
+
+create Procedure InsertBorrowed
+    (IN book_id int, IN userId int, IN borrowed_at TIMESTAMP, IN expiration_date TIMESTAMP, IN returned_at TIMESTAMP)
+BEGIN 
+    IF has_unpaid_fine(userID) THEN
+        SET MYSQL_ERRNO = 9999, MESSAGE_TEXT = "Cannot borrow a book, has a fine";
+    END IF;
+END;
