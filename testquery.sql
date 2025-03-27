40649a9e9f9f6eaf649407df768b6aaa912cfd6d
Nicholas Lentfer Tachibana Kristiansen <nicholas@kristiansenz.com>
2025-03-27 09:49:29 +0100

    more assignment 6

--- testquery.sql
+++ testquery.sql
@@ -1,11 +1,27 @@
 -- 6.1: A table of all borrowed books
--- Active: 1738836473627@@127.0.0.1@3306@db_project
+-- Active: 1743060890162@@127.0.0.1@3306@library_db306@db_project
 SELECT b.*, u.* from borrowed bo
 LEFT JOIN bookcopies bk ON bo.book_id = bk.book_id 
 LEFT JOIN book b ON bk.ISBN = b.ISBN
 LEFT JOIN users u ON bo.userId = u.userid;
 
--- 6.2 A table of all books that have been borrowed by a specific user
+
+-- 6.2: A table of all users that have borrowed a specific book (ISBN)
+select book.ISBN, book.title, users.name, users.email from book
+left join bookcopies on book.ISBN = bookcopies.ISBN
+left join borrowed on bookcopies.book_id = borrowed.book_id
+left join users on borrowed.userId = users.userid
+where book.ISBN = '978-0345391803';
+
+-- 6.3 A table of the total unpaid fines for each user
+select book.title, users.*, fine.* from users 
+join fine on users.userid = fine.userid 
+join bookcopies on fine.book_id = bookcopies.book_id 
+join book on bookcopies.ISBN = book.ISBN
+where fine.paid = 0;
+
+
+-- Amount of books borrowed by a user
 delimiter $$
 CREATE FUNCTION amount_of_books_borrowed(vuserid VARCHAR(15))
 RETURNS INT
